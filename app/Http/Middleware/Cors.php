f7548feb9a6aa82181b411113f013060988958ae
unknown <genedymohamed96@gmail.com>
2018-11-19 00:01:14 +0200

    cors modify

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -23,7 +23,6 @@ class Cors
             // Some security CORS concerns
             //->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-            //->header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With')
             ->header('Access-Control-Allow-Headers','X-Requested-With,content-type')
             ->header('Access-Control-Allow-Credentials', 'true')
             ->header('Access-Control-Max-Age', '10000');
