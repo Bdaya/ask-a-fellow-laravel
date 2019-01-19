68ed143ca3f37b01116e4680018401b81323bc23
GenedyMohamed <genedymohamed96@gmail.com>
2019-01-19 19:47:03 +0200

    added changes for badge.blade

--- resources/views/admin/badge.blade.php
+++ resources/views/admin/badge.blade.php
@@ -18,7 +18,8 @@ $users = $users->skip($page * $take)->take($take)->get();
 @section('content')
     <div class="container" style="width: 90%; padding-left: 50px; ">
         @foreach($users as $user)
-            <div class="media center-block" style="max-width: 300px;">
+            <div class="media center-block" style="max-width: 500px; border: 2px solid orange;">
+                <br>
                 <div class="media-left">
                     <a href="#">
                         @if($user->profile_picture)
@@ -30,7 +31,7 @@ $users = $users->skip($page * $take)->take($take)->get();
                 </div>
                 <div class="media-body">
                     <h4 class="media-heading"><a
-                                href="{{url('user/'.$user->id)}}">{{$user->first_name.' '.$user->last_name}}  {{$user->email}}</a>
+                                href="{{url('user/'.$user->id)}}">Name: {{$user->first_name.' '.$user->last_name}}<br>Email: {{$user->email}}</a>
                     </h4>
                     @if($user->verified_badge ==0)
                     <form method="post" action="{{url('admin/add_badge/'.$user->id)}}">
@@ -50,7 +51,43 @@ $users = $users->skip($page * $take)->take($take)->get();
                         </form>
                     @endif
                 </div>
+                <br>
+                @if($user->verified_badge ==1)
+                <h3> Verified User Courses:
+                <ul style="list-style-type: square;">
+                    @foreach($verified_users_courses as $course)
+                        @if($user->id == $course['user_id'])
+                        <li>{{$courses[$course['course_id']-1]['course_code']}}</li>
+                        @endif
+                    @endforeach
+                </ul>
+               
+                  <form method="post" action="{{url('admin/verified_add_course/'.$user->id)}}">
+                    <select class="form-control" name="item_id" id="item_id">
+                        @foreach($courses as $course)
+                            <option value="{{$course['id']}}">{{$course['course_code']}}</option>
+                        @endforeach
+                    </select>
+                    <br>          
+                  <div class="form-group">
+                    <div class="col-md-4">
+                        <button id="add_badge" name="add_badge" class="btn btn-primary">Add Course</button>
+                    </div>
+                    <div class="col-md-8">  
+                        <button id="add_badge" name="add_badge" class="btn btn-danger">Remove Course</button>
+                    </div>
+                </div>
+            </form>
+                {{--  <div class="form-group">
+                    <div class="col-md-7">
+                        <button id="add_badge" name="add_badge" class="btn btn-danger">Remove Course</button>
+                    </div>
+                </div>  --}}
+                <br><br>
+                @endif
+                
             </div>
+            <br>
         @endforeach
 
 
